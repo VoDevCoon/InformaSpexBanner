f13f274a0a7db8404dc0ab3e45fe6818dda4afa6
multipps@hotmail.com <multipps@hotmail.com>
2017-04-10 21:25:58 +1000

    ctext front function

--- InformaSpexBanner/wwwroot/js/site.js
+++ InformaSpexBanner/wwwroot/js/site.js
@@ -1,2 +1,27 @@
 ﻿// Write your Javascript code.
+$(document).ready(function(){
 
+  $("#ctext").draggable({
+    stop: function(){
+    var position = $(this).position();
+    $("#posxInput").val(position.left);
+    $("#posyInput").val(position.top);
+
+    console.log("x:"+$("#posxInput").val()+"|y:"+$("#posyInput").val());
+    }
+  });
+  
+  $("#ctextInput").on("keyup paste", function() {
+    $("#ctext").text($(this).val());
+  });
+
+  $("#fsizeInput").on("input", function(){
+    var fsize = $(this).val();
+    $("#ctext").css("font-size",fsize+"px")
+  });
+
+  $("#fcolorInput").change(function(){
+    $("#ctext").css('color', $(this).val());
+  });
+
+  });
