ed087adfdc6476ec405badf212089f1b22634f68
VoDevCoon <victorzhouau@gmail.com>
2017-04-21 14:17:40 +1000

    pre-release v0.91

--- InformaSpexBanner/wwwroot/js/site.js
+++ InformaSpexBanner/wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿// Write your Javascript code.
+﻿﻿// Write your Javascript code.
 $(document).ready(function(){
 /* Edit Banner View */
     //initaillise UI		
@@ -29,8 +29,8 @@ $(document).ready(function(){
     	stop: function(){
       	var textPos = $(this).position();
     	var imagePos = $("#bimage").position();
-    	$("#posxInput").val(textPos.left - imagePos.left);
-    	$("#posyInput").val(textPos.top - imagePos.top);
+    	$("#posxInput").val(textPos.left - imagePos.left+2); //2 is the padding
+    	$("#posyInput").val(textPos.top - imagePos.top+2);
     	}
     });
   
@@ -58,36 +58,5 @@ $(document).ready(function(){
     	image.src = $(modalId).find(".thumbnail").attr("src");
     	$(this).find(".imgWidth").text("Width: "+image.width+"px");
     	$(this).find(".imgHeight").text("Height: "+image.height+"px");
-    });
-
-/* Client Download View */
-    $(".download").click(function(){
-    	var img = $(this).attr("data-filename");
-    	var imgsrc = $(img).find(".thumbnail").attr("src");
-
-	    // atob to base64_decode the data-URI
-	    var image_data = atob(imgsrc.split(',')[1]);
-	    // Use typed arrays to convert the binary data to a Blob
-	    var arraybuffer = new ArrayBuffer(image_data.length);
-	    var view = new Uint8Array(arraybuffer);
-	    for (var i=0; i<image_data.length; i++) {
-	        view[i] = image_data.charCodeAt(i) & 0xff;
-	    }
-	    try {
-	        // This is the recommended method:
-	        var blob = new Blob([arraybuffer], {type: 'application/octet-stream'});
-	    } catch (e) {
-	        // The BlobBuilder API has been deprecated in favour of Blob, but older
-	        // browsers don't know about the Blob constructor
-	        // IE10 also supports BlobBuilder, but since the `Blob` constructor
-	        //  also works, there's no need to add `MSBlobBuilder`.
-	        var bb = new (window.WebKitBlobBuilder || window.MozBlobBuilder);
-	        bb.append(arraybuffer);
-	        var blob = bb.getBlob('application/octet-stream'); // <-- Here's the Blob
-	    }
-
-	    // Use the URL object to create a temporary URL
-	    var url = (window.webkitURL || window.URL).createObjectURL(blob);
-	    location.href = url; // <-- Download!
     });
   });
